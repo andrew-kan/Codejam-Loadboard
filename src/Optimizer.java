0fdd694807104017618eeed555fb8eede9349047
Andrew <andrew.m.kan@gmail.com>
2022-02-26 16:15:56 -0500

    Message here

--- src/Optimizer.java
+++ src/Optimizer.java
@@ -9,7 +9,13 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class Optimizer {
+    static double speed = 55.0; // MPH
+    static double fuelCost = 0.40; // $/mile
+    static JSONArray loads;
 
+    /**
+     * Geodesic distance formula (Haversine formula). Adapted from Chris Veness.
+     */
     public static double calc_distance(double lat1, double lon1, double lat2, double lon2) {
         double R = 6371000; // metres
         double φ1 = lat1 * Math.PI/180; // φ, λ in radians
@@ -37,31 +43,10 @@ public class Optimizer {
         }
     }
 
-
-    public static void main(String[] args) throws IOException, org.json.simple.parser.ParseException {
-        double speed = 55.0; // MPH
-        double fuelCost = 0.40; // $/mile
-
-        double startLat = 27.961307;
-        double startLon = -82.4493;
-        String startTime = "2022-02-04 08:00:00";
-        String endTime = "2022-02-06 15:00:00";
-        //System.out.println(findDifference(startTime, endTime));
-        // speed*distance < findDifference time
-
-        // for trips in file
-        // find trips with origins within 2 lat, 2 lon within time (deadhead)
-        // get profit (gain - fuelcost)
-        // find most profitable trip
-        // go on trip - add to array
-        // change time
-        // repeat
-
+    public static ArrayList<Object> findLoads(double startLat, double startLon, String startTime) throws IOException, org.json.simple.parser.ParseException {
         // Parse load data file
-        JSONParser parser = new JSONParser();
-        JSONArray a = (JSONArray) parser.parse(new FileReader("123Loadboard_CodeJam_2022_dataset.json"));
         ArrayList<Object> possibleLoads = new ArrayList<>();
-        for (Object o : a) {        // for each available load, get its data
+        for (Object o : loads) {        // for each available load, get its data
             JSONObject trip = (JSONObject) o;
             long id = (long) trip.get("load_id");
             double orgLat = (double) trip.get("origin_latitude");
@@ -69,26 +54,50 @@ public class Optimizer {
             double destLat = (double) trip.get("destination_latitude");
             double destLon = (double) trip.get("destination_longitude");
             String pickupTime = (String) trip.get("pickup_date_time");
-            pickupTime = pickupTime.substring(0,10) + " " + pickupTime.substring(11,19);    // fixes weird time format in JSON
+            pickupTime = pickupTime.substring(0, 10) + " " + pickupTime.substring(11, 19);    // fixes weird time format in JSON
             long amount = (long) trip.get("amount");
 
             // Consider only loads in nearby area
-            if (Math.abs(orgLat-startLat) < 2 && Math.abs(orgLon-startLon) < 2) {
+            if (Math.abs(orgLat - startLat) < 2 && Math.abs(orgLon - startLon) < 2) {
                 // Consider only profitable loads
                 double deadDist = calc_distance(startLat, startLon, orgLat, orgLon);
                 double loadDist = calc_distance(orgLat, orgLon, destLat, destLon);
-                double profit = amount - loadDist*fuelCost - deadDist*fuelCost;
+                double profit = amount - loadDist * fuelCost - deadDist * fuelCost;
                 if (profit > 0) {
                     // Check if truck can make pickup time
                     double timeDiff = findTimeDiff(startTime, pickupTime);
-                    if (timeDiff >= deadDist*speed) {
+                    if (timeDiff >= deadDist * speed) {
                         possibleLoads.add(o);   // add to list of possible loads
-                        System.out.println("ID: "+id+"\tProfit: $" + profit);
                     }
                 }
             }
         }
+        return possibleLoads;
+    }
 
+    public static void getOptimalRoute(JSONObject driver) throws IOException, org.json.simple.parser.ParseException {
+
+        long id = (long) driver.get("input_trip_id");
+        double startLat = (double) driver.get("start_latitude");
+        double startLon = (double) driver.get("start_longitude");
+        String startTime = (String) driver.get("start_time");
+        String endTime = (String) driver.get("max_destination_time");
+        ArrayList<Object> initial = findLoads(startLat, startLon, startTime);
+        if (initial.size() > 0){
+            System.out.println(initial.get(0));
+        }
+    }
+
+    public static void main(String[] args) throws IOException, org.json.simple.parser.ParseException {
+        JSONParser parser = new JSONParser();
+        JSONArray in = (JSONArray) parser.parse(new FileReader("123Loadboard_CodeJam_2022_input_sample.json"));
+        loads = (JSONArray) parser.parse(new FileReader("123Loadboard_CodeJam_2022_dataset.json"));
+
+        //for (Object o : in) {        // for each input driver, get its data
+        //  JSONObject driver = (JSONObject) o;
+        JSONObject driver = (JSONObject) in.get(0);
+        getOptimalRoute(driver);
+    //    }
     }
 
 
