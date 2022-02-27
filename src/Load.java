2978083f308389bc3abc9d8abb5b4faf546d2966
Andrew <andrew.m.kan@gmail.com>
2022-02-26 20:44:01 -0500

    Initial

---
+++ src/Load.java
@@ -0,0 +1,35 @@
+public class Load {
+    private final long id;
+    private final double profit;
+    private final Object o;
+    private final double orgLat;
+    private final double orgLon;
+    private final double destLat;
+    private final double destLon;
+    private final String pickupTime;
+    private final String endTime;
+
+
+    public Load(Object o, long id, double profit, double orgLat, double orgLon, double destLat, double destLon, String pickupTime, String endTime) {
+        this.o = o;
+        this.id = id;
+        this.profit = profit;
+        this.orgLat = orgLat;
+        this.orgLon = orgLon;
+        this.destLat = destLat;
+        this.destLon = destLon;
+        this.pickupTime = pickupTime;
+        this.endTime = endTime;
+    }
+
+    public Object getO() { return o; }
+    public long getID() { return id; }
+    public double getProfit() { return profit; }
+    public double getOrgLat() { return orgLat; }
+    public double getOrgLon() { return orgLon; }
+    public double getDestLat() { return destLat; }
+    public double getDestLon() { return destLon; }
+    public String getPickupTime() { return pickupTime; }
+    public String getEndTime() { return endTime; }
+
+}
--- src/Optimizer.java
+++ src/Optimizer.java
@@ -2,7 +2,9 @@ import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedList;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -29,7 +31,7 @@ public class Optimizer {
         return d / 1609.34; // in miles
     }
 
-    static double findTimeDiff(String start_date, String end_date) {
+    public static double findTimeDiff(String start_date, String end_date) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         try {
             Date d1 = sdf.parse(start_date);
@@ -43,9 +45,21 @@ public class Optimizer {
         }
     }
 
-    public static ArrayList<Object> findLoads(double startLat, double startLon, String startTime) throws IOException, org.json.simple.parser.ParseException {
+    public static String addTime(String start, double hours) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date d = sdf.parse(start);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d);
+        cal.add(Calendar.HOUR, (int) hours);
+        cal.add(Calendar.MINUTE, (int) hours%1 * 60);
+        cal.add(Calendar.SECOND, (int) hours%1%60 * 60);
+        String newTime = sdf.format(cal.getTime());
+        return newTime;
+    }
+
+    public static ArrayList<Load> findLoads(double startLat, double startLon, String startTime, String maxTime) throws ParseException {
         // Parse load data file
-        ArrayList<Object> possibleLoads = new ArrayList<>();
+        ArrayList<Load> possibleLoads = new ArrayList<>();
         for (Object o : loads) {        // for each available load, get its data
             JSONObject trip = (JSONObject) o;
             long id = (long) trip.get("load_id");
@@ -54,7 +68,7 @@ public class Optimizer {
             double destLat = (double) trip.get("destination_latitude");
             double destLon = (double) trip.get("destination_longitude");
             String pickupTime = (String) trip.get("pickup_date_time");
-            pickupTime = pickupTime.substring(0, 10) + " " + pickupTime.substring(11, 19);    // fixes weird time format in JSON
+            pickupTime = pickupTime.substring(0, 10) + " " + pickupTime.substring(11, 19);    // fixes UTC time format in JSON
             long amount = (long) trip.get("amount");
 
             // Consider only loads in nearby area
@@ -64,10 +78,13 @@ public class Optimizer {
                 double loadDist = calc_distance(orgLat, orgLon, destLat, destLon);
                 double profit = amount - loadDist * fuelCost - deadDist * fuelCost;
                 if (profit > 0) {
-                    // Check if truck can make pickup time
-                    double timeDiff = findTimeDiff(startTime, pickupTime);
-                    if (timeDiff >= deadDist * speed) {
-                        possibleLoads.add(o);   // add to list of possible loads
+                    // Check if truck can make pickup time and if time not too far away
+                    double timeDiffPickup = findTimeDiff(startTime, pickupTime);
+                    double timeDiffEnd = findTimeDiff(pickupTime, maxTime);
+                    if (timeDiffPickup >= deadDist/speed && timeDiffEnd >= loadDist/speed) {// && timeDiffPickup <= 12) {
+                        String endTime = addTime(startTime, loadDist/speed);
+                        Load l = new Load(o, id, profit, orgLat, orgLon, destLat, destLon, pickupTime, endTime);
+                        possibleLoads.add(l);   // add to list of possible loads
                     }
                 }
             }
@@ -75,30 +92,63 @@ public class Optimizer {
         return possibleLoads;
     }
 
-    public static void getOptimalRoute(JSONObject driver) throws IOException, org.json.simple.parser.ParseException {
-
-        long id = (long) driver.get("input_trip_id");
-        double startLat = (double) driver.get("start_latitude");
-        double startLon = (double) driver.get("start_longitude");
-        String startTime = (String) driver.get("start_time");
-        String endTime = (String) driver.get("max_destination_time");
-        ArrayList<Object> initial = findLoads(startLat, startLon, startTime);
-        if (initial.size() > 0){
-            System.out.println(initial.get(0));
+    public static void getOptimalRoute(LinkedList<Load> routing, double startLat, double startLon, String startTime, String maxTime) throws IOException, org.json.simple.parser.ParseException, ParseException {
+        //LinkedList<Load> routing = null;
+        ArrayList<Load> loads = findLoads(startLat, startLon, startTime, maxTime);
+        if (loads.size() > 0) {
+            Load maxProf = loads.get(0);
+            for (Load l : loads) {
+                if (l.getProfit() > maxProf.getProfit()) {
+                    maxProf = l;
+                }
+            }
+            routing.add(maxProf);
+            getOptimalRoute(routing, maxProf.getDestLat(), maxProf.getDestLon(), maxProf.getEndTime(), maxTime);
         }
+        //return routing;
     }
 
-    public static void main(String[] args) throws IOException, org.json.simple.parser.ParseException {
+    public static void main(String[] args) throws IOException, org.json.simple.parser.ParseException, ParseException {
         JSONParser parser = new JSONParser();
         JSONArray in = (JSONArray) parser.parse(new FileReader("123Loadboard_CodeJam_2022_input_sample.json"));
         loads = (JSONArray) parser.parse(new FileReader("123Loadboard_CodeJam_2022_dataset.json"));
+        JSONArray tripList = new JSONArray();
+        for (Object o : in) {        // for each input driver, get its data
+            JSONObject driver = (JSONObject) o;
+            //JSONObject driver = (JSONObject) in.get(4);
+            long id = (long) driver.get("input_trip_id");
+            double startLat = (double) driver.get("start_latitude");
+            double startLon = (double) driver.get("start_longitude");
+            String startTime = (String) driver.get("start_time");
+            String maxTime = (String) driver.get("max_destination_time");
+            LinkedList<Load> routing = new LinkedList<>();
+            getOptimalRoute(routing, startLat, startLon, startTime, maxTime);
 
-        //for (Object o : in) {        // for each input driver, get its data
-        //  JSONObject driver = (JSONObject) o;
-        JSONObject driver = (JSONObject) in.get(0);
-        getOptimalRoute(driver);
-    //    }
+            JSONObject trip = new JSONObject();
+            trip.put("input_trip_id", id);
+            double[] loadIDs = new double[routing.size()];
+            if (routing.size() >= 1) {
+                //LinkedList<Load> routing = getOptimalRoute(startLat, startLon, startTime, maxTime);
+                double totalProfit = 0;
+                for (Load l : routing) {
+                    totalProfit += l.getProfit();
+                    System.out.print(l.getID() + ", ");
+                }
+                System.out.println("$" + totalProfit);
+            } else {
+                System.out.println("No routes");
+            }
+            trip.put("load_ids", loadIDs);
+            tripList.add(trip);
+        }
+
+
+        //Write JSON file
+        try (FileWriter file = new FileWriter("output.json")) {
+            file.write(tripList.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-
 }
